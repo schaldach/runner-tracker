24564c2e03e5cff17525be571cdf2a6e4cf3ef97
schaldach <gabimorgado0311@gmail.com>
2022-12-24 00:07:56 -0300

    preparativos

--- components/Track.js
+++ components/Track.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from "react-native";
 import MapView, { Marker, Polyline } from 'react-native-maps';
+import GradientButton from './GradientButton';
 import * as Location from "expo-location";
+import * as TaskManager from 'expo-task-manager';
 import { Dimensions } from 'react-native';
 
 function Track({ navigation }) {
     const [region, setRegion] = useState(null);
     const [coordinates, setCoordinates] = useState(null)
     const [coordinatesTrail, setTrail] = useState([])
+    const [trackType, setTrack] = useState('Livre')
+    const [elapsedDistance, setDistance] = useState(0)
+
     const options = {
         enableHighAccuracy: true,
         timeout: 20000,
         maximumAge: 1000,
     };
+
+    function startTrack(){
+
+    }
 
     useEffect(() => {
         async function getFirstLocation() {
@@ -45,19 +54,25 @@ function Track({ navigation }) {
                     <View>
                         <MapView
                             zoomControlEnabled={true}
-                            // onRegionChange={(region) => setRegion(region)}
                             region={region}
                             mapType='hybrid'
                             style={styles.mapview}
                         >
                             <Marker coordinate={coordinates} />
-                            <Polyline strokeColor={'#FF4B2B'} lineCap='round' strokeWidth={5} coordinates={[{ latitude: -26.9190254, longitude: -48.649837, }, { latitude: -26.9190254, longitude: -48.652837, }, { latitude: -26.9200254, longitude: -48.655837, }]} />
+                            <Polyline geodesic={true} strokeColor={'#FF4B2B'} lineCap='round' strokeWidth={5} coordinates={[{ latitude: -26.9190254, longitude: -48.649837, }, { latitude: -26.9190254, longitude: -48.652837, }, { latitude: -26.9200254, longitude: -48.655837, }]} />
                         </MapView>
                         <Text style={styles.text}>Longitude: {region.longitude}</Text>
                         <Text style={styles.text}>Latitude: {region.latitude}</Text>
-                    </View>)
+                        <Text style={styles.text}>Você percorreu:</Text>
+                        <Text style={styles.textDisplay}>{elapsedDistance}m</Text>
+                        <GradientButton text='COMEÇAR CORRIDA' onPress={() => startTrack()}/>
+
+                    </View>
+                )
                 :
-                (<Text style={styles.text}>Loading...</Text>)
+                (
+                    <Text style={styles.text}>Loading...</Text>
+                )
             }
         </View>
     );
@@ -78,6 +93,11 @@ const styles = StyleSheet.create({
     text: {
         fontFamily: 'Helvetica',
         fontSize: 20
+    },
+    textDisplay: {
+        fontFamily: 'Helvetica',
+        fontSize: 20,
+        color: '#FF4B2B'
     }
 });
 
